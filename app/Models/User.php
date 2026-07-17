4edfe4067ef9b824e656a539b9c2aa623a87b9cd
Sourav Sarkar <isourav2018@gmail.com>
2026-07-17 07:40:56 +0530

    test workflow

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,8 +42,7 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    protected static function booted()
-    {
+    protected static function booted(){
         // TODO: need to code later
     }
 }
